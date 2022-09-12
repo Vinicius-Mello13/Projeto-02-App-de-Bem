5f8e7d487e6235a6592c5b69bef5343c7201168b
Giuliana Cabral <cabral.giu@hotmail.com>
2022-09-12 18:05:06 -0300

    Atualização 12set 18h05

--- app-de-bem/src/App.js
+++ app-de-bem/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Loading } from "./pages/1. Loading";
+import { Username } from "./pages/3.1 Sign Up";
+import { SignUp } from "./pages/3.2 Sign Up";
+import { PrivateTalk } from "./pages/4. Private Talk";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
-  );
-}
+    return (
+      <Routes>
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/username" element={<Username />} /> 
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/privatetalk" element={<PrivateTalk />} />
+      </Routes>
+    )
+  }
 
